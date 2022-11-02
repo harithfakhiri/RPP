a89e1577e1b9cb522513e10f149e831450a94830
ibnuhafizh <hafizhputraa@gmail.com>
2022-11-02 23:44:24 +0700

    integrate html to flask

--- predictflask/envpredict/hello.py
+++ predictflask/envpredict/hello.py
@@ -1,5 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from dummy import *
+
 app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    return 'Hello world!'
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if (request.method == 'GET'):
+        return render_template("index.html")
+    
+    if (request.method == 'POST'):
+        arr_input = request.form.get('text')
+        prediction("./finalized_model.sav", arr_input)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run()
