e476ea77c3b49dc470ab819690ed4d8ebfbbb359
findraya <68522767+findraya@users.noreply.github.com>
2022-11-03 09:20:03 +0700

    connect to flask

--- predictflask/envpredict/app.py
+++ predictflask/envpredict/app.py
@@ -1,17 +1,34 @@
 from flask import Flask, render_template, request, redirect, url_for
 from dummy import *
+import sys
 
 app = Flask(__name__)
+pred = -1
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if (request.method == 'GET'):
-        return render_template("index.html")
+        global pred
+        if pred == 0:
+            pred_result = "M"
+        elif pred == 1:
+            pred_result = "B"
+        else:
+            pred_result = " Belum melakukan prediksi"
+        return render_template("index.html", prediction=pred_result)
     
     if (request.method == 'POST'):
-        arr_input = request.form.get('text')
-        prediction("./finalized_model.sav", arr_input)
+        inputs = []
+        res = request.form.to_dict()
+        
+        for key, value in res.items():
+            inputs.append(float(value))
+        arr_input = [inputs]
+
+        # print(arr_input, file=sys.stdout)
+        pred = prediction('./predictflask/envpredict/finalized_model.sav', arr_input)
+        # print(pred, file=sys.stdout)
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
