54f52ef386cbd56c88e7ef40b5ac8dcdfe243f91
harithfakhiri <13519161@std.stei.itb.ac.id>
2022-11-04 01:52:29 +0700

    final harusnya

--- predictflask/envpredict/app.py
+++ predictflask/envpredict/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-from dummy import *
+from utils import *
 import sys
 
 app = Flask(__name__)
@@ -9,13 +9,17 @@ pred = -1
 def index():
     if (request.method == 'GET'):
         global pred
+
         if pred == 0:
-            pred_result = "M"
+            pred_result = "You Got a Malignant"
+            desc = "Malignant neoplasms are cancerous tumors. They develop when cells grow and divide more than they should. Malignant neoplasms can spread to nearby tissues and to distant parts of your body. Treatment options may include surgery, chemotherapy or radiation therapy. Early detection is key, so be sure to attend all recommended cancer screenings."
         elif pred == 1:
-            pred_result = "B"
+            pred_result = "You Got a Benign"
+            desc="A benign tumor is an abnormal but noncancerous collection of cells. It can form anywhere on or in your body when cells multiply more than they should or don’t die when they should. A benign tumor is not malignant. It grows more slowly, has even borders and doesn’t spread to other parts of your body. Many benign tumors don’t require treatment."
         else:
             pred_result = " Belum melakukan prediksi"
-        return render_template("index.html", prediction=pred_result)
+            desc = ""
+        return render_template("index.html", prediction=pred_result, description=desc)
     
     if (request.method == 'POST'):
         inputs = []
